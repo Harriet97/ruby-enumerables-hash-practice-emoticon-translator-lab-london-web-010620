18b0a7250a423aaba6b31d17429c36399d14360c
Harriet Bicknell <harrietindubai@gmail.com>
2019-12-30 20:58:39 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -3,10 +3,10 @@ require "yaml"
 
 def load_library(file_path)
   library = YAML.load_file(file_path)
-  result = {:get_meaning = {}, "get_emoticon" => {} }
+  result = {get_meaning: {}, get_emoticon: {} }
   library.each do |meaning, emoticons|
-    result["get_meaning"][emoticons[1]] = meaning  #japanese keys
-    result["get_emoticon"][emoticons[0]] = emoticons[1] #english keys that point to japanese equivalent
+    result[:get_meaning][emoticons[1]] = meaning  #japanese keys
+    result[:get_emoticon][emoticons[0]] = emoticons[1] #english keys that point to japanese equivalent
   end
   result
 end
